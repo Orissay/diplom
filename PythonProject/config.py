259e6f13ebdc899f4c13913a97de704c8499b939
NEKRASOV KOSTIANTYN <145330291+Orissay@users.noreply.github.com>
2025-05-26 16:18:09 +0300

    Add files via upload

---
+++ PythonProject/config.py
@@ -0,0 +1,10 @@
+
+# Настройки Supabase
+SUPABASE_URL = "https://hxowoktqmcgrckptjvnz.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4b3dva3RxbWNncmNrcHRqdm56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMDc0MzgsImV4cCI6MjA2Mjc4MzQzOH0.znG6XuvFzHE_iIpl3j79UW7dJORB3UhF-qAHvuSrOiY"
+
+# Настройки Telegram бота
+BOT_TOKEN = "7244593523:AAGhMM2XuHgKQ0zII5zE0xNSe5mS5-N0vWw"
+
+# API Nova Poshta
+NOVA_POSHTA_API_KEY = "78bdffdabccd762699b69916b9f3d6c3"
